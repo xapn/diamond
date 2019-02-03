6ab6a83ca5fef8ba71a72aa9029ab870bb20b5ad
Xavier Pigeon <xpn.projects@gmail.com>
2019-02-03 17:05:16 +0100

    Refactor tests to nest invalid test cases.

--- src/test/java/diamond/DiamondTest.java
+++ src/test/java/diamond/DiamondTest.java
@@ -1,5 +1,6 @@
 package diamond;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static testasyouthink.TestAsYouThink.resultOf;
@@ -8,24 +9,8 @@ import static testasyouthink.TestAsYouThink.whenOutsideOperatingConditions;
 class DiamondTest {
 
     @Test
-    void should_fail_to_create_a_diamond_given_no_letter() {
-        whenOutsideOperatingConditions(() -> Diamond.of(""))
-                .thenItFails()
-                .becauseOf(IllegalArgumentException.class)
-                .withMessage("Letter missing!");
-    }
-
-    @Test
     void should_create_a_diamond_given_A() {
         resultOf(() -> Diamond.of("A")).isEqualTo("A");
-    }
-
-    @Test
-    void should_fail_to_create_a_diamond_given_nil() {
-        whenOutsideOperatingConditions(() -> Diamond.of(null))
-                .thenItFails()
-                .becauseOf(IllegalArgumentException.class)
-                .withMessage("Argument value missing!");
     }
 
     @Test
@@ -42,5 +27,25 @@ class DiamondTest {
                 + "C   C\n" //
                 + " B B\n" //
                 + "  A");
+    }
+
+    @Nested
+    class Given_an_invalid_argument {
+
+        @Test
+        void should_fail_to_create_a_diamond_given_nil() {
+            whenOutsideOperatingConditions(() -> Diamond.of(null))
+                    .thenItFails()
+                    .becauseOf(IllegalArgumentException.class)
+                    .withMessage("Argument value missing!");
+        }
+
+        @Test
+        void should_fail_to_create_a_diamond_given_no_letter() {
+            whenOutsideOperatingConditions(() -> Diamond.of(""))
+                    .thenItFails()
+                    .becauseOf(IllegalArgumentException.class)
+                    .withMessage("Letter missing!");
+        }
     }
 }
