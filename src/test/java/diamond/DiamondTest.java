31365a99a73aa2e6c57b7fdc0409c038753c428f
Xavier Pigeon <xpn.projects@gmail.com>
2019-02-03 16:08:45 +0100

    It should return AnBBnCCnBBnA given 'C'.

--- src/test/java/diamond/DiamondTest.java
+++ src/test/java/diamond/DiamondTest.java
@@ -2,8 +2,14 @@ package diamond;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static java.lang.String.valueOf;
+import static java.util.Collections.reverse;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.IntStream.rangeClosed;
 import static testasyouthink.TestAsYouThink.resultOf;
 import static testasyouthink.TestAsYouThink.whenOutsideOperatingConditions;
@@ -13,12 +19,18 @@ class DiamondTest {
     private static String diamondOf(String letter) {
         if (letter == null) {
             throw new IllegalArgumentException("Argument value missing!");
-        } else if (letter.isEmpty()) {
+        } else if (letter.isEmpty() || "A".equals(letter)) {
             return letter;
         } else {
-            return rangeClosed((int) 'A', letter.codePointAt(0))
+            List<String> atFirst = rangeClosed((int) 'A', letter.codePointAt(0))
                     .mapToObj(code -> code == 65 ? "A" : valueOf((char) code) + valueOf((char) code))
-                    .collect(joining("\n", "", "A".equals(letter) ? "" : "\nA"));
+                    .collect(toList());
+            List<String> atLast = new ArrayList<>(atFirst.subList(0, atFirst.size() - 1));
+            reverse(atLast);
+            return Stream
+                    .of(atFirst, atLast)
+                    .flatMap(List::stream)
+                    .collect(joining("\n"));
         }
     }
 
@@ -43,5 +55,10 @@ class DiamondTest {
     @Test
     void should_return_AnBBnA_given_B() {
         resultOf(() -> diamondOf("B")).isEqualTo("A\nBB\nA");
+    }
+
+    @Test
+    void should_return_AnBBnCCnBBnA_given_C() {
+        resultOf(() -> diamondOf("C")).isEqualTo("A\nBB\nCC\nBB\nA");
     }
 }
