e704603d8f40b3df04155c66ae376121bad151a4
Xavier Pigeon <xpn.projects@gmail.com>
2019-02-03 15:01:12 +0100

    It should create a diamond given 'A'.

--- src/test/java/diamond/DiamondTest.java
+++ src/test/java/diamond/DiamondTest.java
@@ -7,11 +7,16 @@ import static testasyouthink.TestAsYouThink.resultOf;
 class DiamondTest {
 
     private static String diamondOf(String letter) {
-        return "";
+        return letter;
     }
 
     @Test
     void should_create_nothing_given_no_letter() {
         resultOf(() -> diamondOf("")).isEqualTo("");
+    }
+
+    @Test
+    void should_create_a_diamond_given_A() {
+        resultOf(() -> diamondOf("A")).isEqualTo("A");
     }
 }
